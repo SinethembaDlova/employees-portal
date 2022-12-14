dc9cc66859b8ddcbf553c263ae8f09498dc181b4
Sinethemba <sinethemba@wealthmigrate.com>
2022-12-14 18:59:16 +0200

    created a basic project structure I might need.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import emptyState from './assets/emptyState.JPG';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={emptyState} alt="logo" />
+        <p>There is nothing here.</p>
+        Create a new employee by clicking the New Employee butoon to get started
       </header>
     </div>
   );
