589efcb4b2eddb535fab36bb0257a11097e49154
Sinethemba <sinethemba@wealthmigrate.com>
2023-01-20 03:12:00 +0200

    created a loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { LoaderContainer, LoaderOverlay } from './index.style';
+
+const Loader = () => {
+  return (
+    <LoaderOverlay>
+      <LoaderContainer />
+    </LoaderOverlay>
+  );
+
+};
+
+export default Loader;
+
