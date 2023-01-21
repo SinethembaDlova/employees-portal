9bac4bccd5edd05a1fd4aecb356a5696449e1c99
Sinethemba <sinethemba@wealthmigrate.com>
2023-01-21 21:08:52 +0200

    created a loader component

---
+++ src/components/Notification/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { VARIANT } from '../../constants';
+import {
+  NotificationContainer,
+  IconTextWrapper,
+  IconWrapper,
+  TextWrapper
+} from './index.style';
+
+const Alert = ({ variant, message, noBorder, ...restProps }) => {
+  return (
+    <NotificationContainer
+      variant={variant}
+      message={message}
+      noBorder={noBorder}
+      {...restProps}
+    >
+      <IconTextWrapper>
+        <IconWrapper>
+          {variant === VARIANT.SUCCESS && (
+            <i className="material-icons">check circle</i>
+          )}
+          {variant === VARIANT.WARNING && (
+            <i className="material-icons">warning</i>
+          )}
+          {variant === VARIANT.ERROR && (
+            <i className="material-icons">error</i>
+          )}
+        </IconWrapper>
+        <TextWrapper>
+          <div>{message}</div>
+        </TextWrapper>
+      </IconTextWrapper>
+    </NotificationContainer>
+  );
+};
+
+export default Alert;
---
+++ src/components/Notification/index.style.js
@@ -0,0 +1,44 @@
+import styled, { css } from 'styled-components';
+
+const NotificationContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  display: flex;
+  padding: 10px 20px;
+  border-radius: ${props => (props.noBorder ? '0px' : '10px')};
+  ${props => {
+    if (props.variant === 'success') {
+      return css`
+        background: green;
+        color: #fff;
+      `;
+    }
+    if (props.variant === 'warning') {
+      return css`
+        background: yellow;
+      `;
+    }
+    if (props.variant === 'error') {
+      return css`
+        background: red;
+        color: #fff;
+      `;
+    }
+  }};
+`;
+
+const IconTextWrapper = styled.div`
+  display: flex;
+`;
+
+const IconWrapper = styled.div`
+  padding-right: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const TextWrapper = styled.div`
+  display: flex;
+`;
+
+export { NotificationContainer,  IconTextWrapper, IconWrapper,  TextWrapper }
