47ac456a0d97a2b2c8c67dcbc8310da7179f0534
Sinethemba <sinethemba@wealthmigrate.com>
2023-03-09 03:38:53 +0200

    moved empty state state into a images/state folder

--- src/components/emptyState/index.js
+++ src/components/emptyState/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Container, Image } from './index.style';
-import emptyState from '../../assets/emptyState.JPG';
+import empty from '../../assets/images/state';
 
 const EmptyState = () => {
   return (
     <Container>
-      <Image src={emptyState} alt="empty" />
+      <Image src={empty} alt="empty" />
       <p>There is nothing here.</p>
       <p>Create a new employee by clicking the New Employee button.</p>
     </Container>
