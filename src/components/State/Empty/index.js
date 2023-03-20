9be4216bf3aa4d896a1262ee82ee7cdb6df1c5b7
Sinethemba <sinethemba@wealthmigrate.com>
2023-03-21 01:43:59 +0200

    removed the email

--- src/components/State/Empty/index.js
+++ src/components/State/Empty/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Container, Image } from './index.style';
+import { Container } from './index.style';
 // import empty from '../../../assets/images/state/empty.jpg';
 
 const EmptyState = () => {
   return (
     <Container>
-      <Image src={require('../../../assets/images/state/empty.jpg')} alt="empty" />
+      {/* <Image src={require('./assets/images/state/empty.jpg')} alt="empty" /> */}
       <p>There is nothing here.</p>
       <p>Create a new employee by clicking the New Employee button.</p>
     </Container>
